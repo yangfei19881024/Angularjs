6c00eb2e77cd3e7e4076950bac1f9f4d07f75245
yangfei <30797172@qq.com>
2015-04-06 10:28:12 +0800

    q

--- Base/route-controller模块化/controller/ctrl.js
+++ Base/route-controller模块化/controller/ctrl.js
@@ -2,13 +2,35 @@ var ctrlModule = angular.module("ctrlModule",[]);
 
 ctrlModule.controller("listController",function($scope,dataService){
 
-	dataService.loadData()
-		.success(function(data){
-			$scope.names = data;
-		});
+	var promise = dataService.loadData();
+
+	promise.then(function(data){
+		$scope.names = data;
+	},function(){
+		//error
+	});
+
+	console.log($scope.names)
 
 });
 
-ctrlModule.controller("detailController",function($scope,$routeParams){
-	console.log($routeParams);
+ctrlModule.controller("detailController",function($scope,$routeParams,dataService){
+	
+	// var index = $routeParams.id;
+	// $scope.detail = $scope
+
+	var promise = dataService.loadData();
+
+	promise.then(function(data){
+		$scope.names = data;
+	},function(){
+		//error
+	})
+
+	// var index = $routeParams.id;
+	// $scope.detail = $scope.names.index(index);
+
+	console.log($scope.names);
+	
+
 });
--- Base/route-controller模块化/service/dataService.js
+++ Base/route-controller模块化/service/dataService.js
@@ -1,13 +1,24 @@
 var serviceModel = angular.module("serviceModel",[]);
 
-serviceModel.service("dataService",function($http){
+serviceModel.service("dataService",function($http,$q){
 	
 	return {
 		loadData:function(parm){
-			return $http({
+			
+			var defered = $q.defer();
+			$http({
 				method:"post",
 				url:"./data.json"
+			}).
+			success(function(data){
+				defered.resolve(data);
+			}).
+			error(function(data){
+				defered.reject(data);
 			});
+
+			return defered.promise;
+
 		}
 	}
 
