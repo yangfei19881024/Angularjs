f592193a26e51356cde5896377b09d53cb848b8b
yangfei <30797172@qq.com>
2015-04-12 16:47:17 +0800

    filter-9s

---
+++ Base/filter-9s/filter-orderby.js
@@ -0,0 +1,28 @@
+var myModule = angular.module("myModule",[]);
+
+myModule.controller("myController",["$scope",function($scope){
+
+	$scope.a = "a";
+
+	$scope.childrenArray = [
+	    {name:'kimi4',age:3},
+	    {name:'cindy',age:4},
+	    {name:'anglar',age:4},
+	    {name:'shitou',age:6},
+	    {name:'tiantian',age:5}
+	];
+
+	$scope.func = function(data){
+		return data.age >= 4;
+	}
+
+	$scope.orderFunc = function(){
+		return "-age";
+	}
+}]);
+
+myModule.filter("limits",function(){
+	return function(item,param1,param2){
+		return item.substr(0,param2);
+	}
+})
