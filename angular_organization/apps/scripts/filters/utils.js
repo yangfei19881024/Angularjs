801405614cf25fcd1d919987ddf5c2444525bc90
yangfei <30797172@qq.com>
2015-06-11 21:17:44 +0800

    filter

--- angular_organization/apps/scripts/filters/utils.js
+++ angular_organization/apps/scripts/filters/utils.js
@@ -8,4 +8,24 @@ angular.module("shopApp.filter")
 
     }
 
-  });
+  })
+  .filter("filterUsers",function(){
+    /**
+      注意：这里的 objValue 参数 是 users 这个对象 而不是对象里的item
+
+    **/
+    return function(objValue,param){
+      var list = [];
+
+      angular.forEach(objValue,function(item){
+
+        if( item.age > param ){
+          list.push( item );
+        }
+
+      })
+
+      return list;
+    }
+
+  })
